ab14dd950d23c8ee32a9d5d7a726bc2ca8cfa455
Allen <allenabraham505@gmail.com>
2024-12-16 22:49:20 -0500

    Add IF NOT EXISTS to SQL commands

--- bandhan.sql
+++ bandhan.sql
@@ -1,12 +1,12 @@
 
-CREATE TABLE boards (
+CREATE TABLE IF NOT EXISTS boards (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         date_created DATETIME NOT NULL,
                             matrix_data text NOT NULL,
                         owner_id bigint
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                        username character varying(255) NOT NULL,
                        date_created DATETIME NOT NULL
